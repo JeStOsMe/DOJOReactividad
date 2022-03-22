033df1f655c4a925fc98798de568c7bc8eff9a27
Jeaniel Osorno <jeanielosorno@gmail.com>
2022-03-22 12:07:36 -0500

    Ejercicio 2 en proceso

---
+++ src/main/java/com/dojo/actividad/ActividadChatMalasPalabras.java
@@ -0,0 +1,50 @@
+package com.dojo.actividad;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.dojo.actividad.models.Chat;
+import com.dojo.actividad.models.MalasPalabras;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import reactor.core.publisher.Flux;
+
+@SpringBootApplication
+public class ActividadChatMalasPalabras implements CommandLineRunner{
+
+    public static void main(String[] args) {
+		SpringApplication.run(ActividadApplication.class, args);
+	}
+    
+    @Override
+    public void run(String... args) throws Exception {
+        List<MalasPalabras> malasPalabras = generadorDeMalasPalabras();
+        List<Chat> chat = generadorDeChat();
+
+        Flux.fromIterable(chat)
+            .map(mensaje -> {
+                //Falta la lógica
+            })
+        
+    }
+
+    public List<MalasPalabras> generadorDeMalasPalabras(){
+        List<MalasPalabras> malasPalabras = new ArrayList<>();
+
+        malasPalabras.add(new MalasPalabras("vaso"));
+        
+        return malasPalabras;
+    }
+
+    public List<Chat> generadorDeChat(){
+        List<Chat> chat = new ArrayList<>();
+
+        chat.add(new Chat("Resulta y acontece que este vaso está roto"));
+
+        return chat;
+    }
+    
+}
